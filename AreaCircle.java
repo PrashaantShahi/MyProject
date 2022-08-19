2803498c5b14084d13cf277a0b0bf71fe1d74ba4
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-19 10:52:16 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,16 @@
+package demo.java;
+
+public class AreaCircle {
+
+	public static void main (String args[])
+	{
+		double r,pi,area=0;
+		
+		pi=3.14;
+		r=1.1;
+		
+		area= pi*r*r;
+		
+		System.out.println(area);
+	}
+}
