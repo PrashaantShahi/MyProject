e4a030ef4095dfa42f351e51d21b571884c9fe18
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-19 11:22:43 +0530

    Add files via upload

---
+++ LessOrGreater.java
@@ -0,0 +1,19 @@
+package demo.java;
+
+public class LessOrGreater {
+
+	public static void main(String args[])
+	{
+		int n1=10,n2=20;
+		
+		if(n1>n2)
+		{
+			System.out.println("n1 is greater than n2");
+			
+		}
+		else
+		{
+			System.out.println("n2 is greater");
+		}
+	}
+}
