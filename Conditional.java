656b8fc141271205632fbeb05796bfa21d361a31
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-19 11:03:58 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,13 @@
+package demo.java;
+
+public class Conditional {
+	public static void main(String args[])
+	{
+		int num=2;
+		
+		String res= num>0 ? "It is a positive number" : "It is a negative number";
+		
+		System.out.println(res);
+	}
+
+}
