8ea5aa1a77e7519ef156b813b811aad449a617a1
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-19 13:24:39 +0530

    Add files via upload

---
+++ Divisible.java
@@ -0,0 +1,27 @@
+package demo.java;
+
+public class Divisible {
+	public static void main (String args[])
+	{
+		int num=22;
+		if(num%5==0 && num%3==0)
+		{
+			System.out.println("It is both divisible by 5 and 3 ");
+		}
+		else if(num%5==0)
+		{ 
+			System.out.println("It is divisible by 5 but not 3");
+		}
+		else if(num%3==0)
+			{
+				System.out.println("It is divisible by 3 but not 5");
+			}
+	    else
+			{
+				System.out.println("It is neither divisible by 5 nor 3");
+			}
+
+		
+	
+	}
+}
