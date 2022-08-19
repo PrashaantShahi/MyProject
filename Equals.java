6d535a7c3fac60453c0675689960b9470509d83f
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-19 20:20:49 +0530

    Add files via upload

---
+++ Equals.java
@@ -0,0 +1,19 @@
+package demo.java;
+
+public class Equals {
+		public void main(String args[])
+		{
+			int i=1,j=2,k=3,l=4;
+		
+			
+			if(i==j && j==k && k==l)
+			{
+				System.out.println("Equal");
+			}
+			else
+			{
+				System.out.println("Not Equal");
+			}
+		
+	}
+}
