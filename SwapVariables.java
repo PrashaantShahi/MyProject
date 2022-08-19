c3aeb47692da225d4c732987af296c9cb198a17d
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-19 13:24:07 +0530

    Add files via upload

---
+++ SwapVariables.java
@@ -0,0 +1,28 @@
+package demo.java;
+
+public class SwapVariables {
+	public static void main(String args[])
+	{
+		int a=1,b=2,temp=0;
+		
+		System.out.println("Before Swap");
+		
+		System.out.println("a="+a);
+		
+		System.out.println("b="+b);
+		
+		temp=a;
+		
+		a=b;
+		
+		b=temp;
+		
+		System.out.println("After Swap");
+		
+		System.out.println("a="+a);
+		
+		System.out.println("b="+b);
+		
+		}
+
+}
