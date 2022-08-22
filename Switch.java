13a00be0bcfb84a0207ec4f9277e67e2c3fa87d3
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-22 12:38:24 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,33 @@
+package demo.java;
+
+
+
+public class Switch {
+	public static void main(String args[])
+	{
+		int a=2;
+		switch(a)
+		{
+		case 1:
+			System.out.println("One");
+		break;
+		
+		case 2:
+			System.out.println("Two");
+		break;
+		
+		case 3:
+			System.out.println("Tree");
+		break;
+		
+		default:
+			System.out.println("Invalid");
+		}
+	
+		
+		
+	
+	
+	}
+
+}
