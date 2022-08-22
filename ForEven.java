e76a0eca64810fb841c5b89a5adb1a3720fb54a5
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-22 11:35:49 +0530

    Add files via upload

---
+++ ForEven.java
@@ -0,0 +1,16 @@
+package demo.java;
+
+public class ForEven {
+	public static void main(String args[])
+	{ 
+		int i;
+		for( i=1 ; i<=10;i++)
+		{
+			if (i%2==0) {
+				System.out.println( i);
+			}
+		}
+		  
+	}
+
+}
