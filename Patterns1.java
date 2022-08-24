21becd67e3d4b96222084550fc394762eb1a0293
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-08-24 11:41:30 +0530

    Add files via upload

---
+++ Patterns1.java
@@ -0,0 +1,17 @@
+package demo.java;
+
+public class Patterns1 {
+	public static void main(String args[])
+	{
+	
+		for(int rows=5;rows>0;rows--)
+		{
+			for(int col=0;col<=rows-1;col++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
