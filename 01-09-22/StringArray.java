8d739f233cdbdb3f8992d3f2d4eb168b2efc42ce
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-09-01 13:15:30 +0530

    Add files via upload

---
+++ 01-09-22/StringArray.java
@@ -0,0 +1,14 @@
+package assignment.java;
+
+public class StringArray {
+
+	public static void main(String args[])
+	{
+		String s[]= {"aman","eli","ire","elazer","akash"};
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(s[i]);
+		}
+	}
+}
