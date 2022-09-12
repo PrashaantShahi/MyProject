c79392c3b16111f9550037003fd49ec010e2530c
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-09-12 11:03:22 +0530

    Add files via upload

---
+++ 09-09-2022/BubbleSearch.java
@@ -0,0 +1,15 @@
+/* WAP to input an array in random order and then sort it using bubble 
+ sort and then search for a given element from that sorted array*/
+package newAssignment;
+
+import java.util.Scanner;
+
+public class BubbleSearch {
+
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		
+	}
+}
