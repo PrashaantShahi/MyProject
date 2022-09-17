5b46ef56ad364f9c07f8ba34985c297865389268
PrashaantShahi <111556294+PrashaantShahi@users.noreply.github.com>
2022-09-17 13:15:27 +0530

    Add files via upload

---
+++ 17-09-2022/Countletters.java
@@ -0,0 +1,21 @@
+package newAssignment;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Countletters {
+
+	public static void main(String args[])throws IOException{
+		FileInputStream fin=new FileInputStream("test.txt");
+		
+		int i;
+		int count=0;
+		while((i=fin.read())!=-1)
+		{
+				count=count+1;
+		}
+		System.out.println(count);
+		
+		fin.close();
+	}
+}
